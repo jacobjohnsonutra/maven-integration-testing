0687e938716a859b4e07ca9b0644bb563243644d
Guillaume Nodet <gnodet@gmail.com>
2021-06-01 17:33:48 +0200

    [MNG-7157] Provide a better design for obtaining artifacts from a MavenProject inside a mojo

---
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7157NewProjectArtifactsApi.java
@@ -0,0 +1,61 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-7157">MNG-7157</a>.
+ */
+public class MavenITmng7157NewProjectArtifactsApi
+    extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng7157NewProjectArtifactsApi()
+    {
+        super( "[4.0.0-alpha-1,)" );
+    }
+
+    /**
+     * Test that ensures the newest-wins conflict resolver is used.
+     */
+    public void testitMNG7157()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7157/dependency" );
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7157/plugin" );
+        verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7157/project" );
+        verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "verify" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+}
